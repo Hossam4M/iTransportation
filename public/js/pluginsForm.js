b89ac8d87454a37818aa4a82308a60e47770bd3f
Hossam4M <36332545+Hossam4M@users.noreply.github.com>
2018-07-24 22:45:48 +0200

    Add files via upload

--- public/js/pluginsForm.js
+++ public/js/pluginsForm.js
@@ -26,7 +26,7 @@ $('#book').click((e)=>{
 let stopNo = 1;
 $('#addStop').click((e)=>{
   e.preventDefault();
-  let html_stop = '<div id="stop_'+stopNo+'" class="form-row"><div class="col-md-6 offset-md-3 mb-3"><input type="text" class="form-control stops" placeholder="add your stop here" name="stops" required></div><div class="col-md-2 text-danger"><h4><i id="delStop_'+stopNo+'" class="fa fa-times deleteStops" aria-hidden="true"></i></h4></div></div><script>$("#delStop_'+stopNo+'").click((e)=>{e.preventDefault();let id = $(e.target).attr("id");let arr = id.split("_");let stopNum = arr[1];$("#stop_"+stopNum).remove();});</script>';
+  let html_stop = '<div id="stop_'+stopNo+'" class="form-row"><div class="col-md-6 offset-md-3"><input type="text" class="form-control stops" placeholder="add your stop here" name="stops" required></div><div class="col-md-2 text-danger text-center"><h2><i id="delStop_'+stopNo+'" class="fa fa-times deleteStops" aria-hidden="true"></i></h2></div></div><script>$("#delStop_'+stopNo+'").click((e)=>{e.preventDefault();let id = $(e.target).attr("id");let arr = id.split("_");let stopNum = arr[1];$("#stop_"+stopNum).remove();});</script>';
   $( html_stop ).insertBefore( "#stopDiv" );
   stopNo++;
 });
