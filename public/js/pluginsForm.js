e982eb9f67c434d4ea3961f701b196f5bd0fdb35
Hossam4M <36332545+Hossam4M@users.noreply.github.com>
2018-07-25 21:13:18 +0200

    Add files via upload

--- public/js/pluginsForm.js
+++ public/js/pluginsForm.js
@@ -1,3 +1,8 @@
+// datepicker
+$( function() {
+    $( "#pDate" ).datepicker();
+  } );
+
 // edit buttons
 $('#edit1').click((e)=>{
   e.preventDefault();
@@ -26,7 +31,7 @@ $('#book').click((e)=>{
 let stopNo = 1;
 $('#addStop').click((e)=>{
   e.preventDefault();
-  let html_stop = '<div id="stop_'+stopNo+'" class="form-row"><div class="col-md-6 offset-md-3"><input type="text" class="form-control stops" placeholder="add your stop here" name="stops" required></div><div class="col-md-2 text-danger text-center"><h2><i id="delStop_'+stopNo+'" class="fa fa-times deleteStops" aria-hidden="true"></i></h2></div></div><script>$("#delStop_'+stopNo+'").click((e)=>{e.preventDefault();let id = $(e.target).attr("id");let arr = id.split("_");let stopNum = arr[1];$("#stop_"+stopNum).remove();});</script>';
+  let html_stop = '<div id="stop_'+stopNo+'" class="form-row"><div class="col-md-6 offset-md-3"><input id="stop'+stopNo+'" type="text" class="form-control stops" placeholder="add your stop here" name="stops" required></div><div class="col-md-2 text-danger text-center"><h2><i id="delStop_'+stopNo+'" class="fa fa-times deleteStops" aria-hidden="true"></i></h2></div></div><script>$("#delStop_'+stopNo+'").click((e)=>{e.preventDefault();let id = $(e.target).attr("id");let arr = id.split("_");let stopNum = arr[1];$("#stop_"+stopNum).remove();});let input = document.getElementById("stop'+stopNo+'");let searchBox = new google.maps.places.SearchBox(input,{componentRestrictions:{country: "us"}});</script>';
   $( html_stop ).insertBefore( "#stopDiv" );
   stopNo++;
 });
